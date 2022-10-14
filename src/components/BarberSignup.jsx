fe132b50996671dd01fdbb478a4f7230243d4539
Julian Rivera <julianarevircoins@gmail.com>
2022-10-13 23:56:37 -0400

    barber sign up

--- src/components/BarberSignup.jsx
+++ src/components/BarberSignup.jsx
@@ -1,33 +1,90 @@
+import { useState } from "react";
+import axios from "axios";
+
 const BarberSignup = () => {
+  const [state, setState] = useState({
+    name: "",
+    password: "",
+    email: "",
+    experience: "",
+    location: "",
+    from: "",
+  });
+
+  const updateState = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/signup/barber", state)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <section className="signup">
-        <form className="authForm">
+        <form className="authForm" onSubmit={handleSubmit}>
           <div className="inputs">
             <label htmlFor="">name: </label>
 
-            <input type="text" placeholder="Enter your name" />
+            <input
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+              onChange={updateState}
+            />
           </div>
           <div className="inputs">
             <label htmlFor="">password: </label>
-            <input type="password" placeholder="Enter your Password" />
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              onChange={updateState}
+              name="password"
+            />
           </div>
           <div className="inputs">
             <label htmlFor="">email: </label>
-            <input type="email" placeholder="Enter your email" />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              onChange={updateState}
+              name="email"
+            />
           </div>
           <div className="inputs">
             <label htmlFor="">experience: </label>
-            <input type="text" placeholder="Enter your experience" />
+            <input
+              type="text"
+              placeholder="Enter your experience"
+              onChange={updateState}
+              name="experience"
+            />
           </div>
           <div className="inputs">
             <label htmlFor="">location: </label>
-            <input type="text" placeholder="Where do you live?" />
+            <input
+              type="text"
+              placeholder="Where do you live?"
+              onChange={updateState}
+              name="location"
+            />
           </div>
           <div className="inputs">
             <label htmlFor="">from: </label>
-            <input type="text" placeholder="Where are you from?" />
+            <input
+              type="text"
+              placeholder="Where are you from?"
+              onChange={updateState}
+              name="from"
+            />
           </div>
+          <button>Sign up</button>
         </form>
       </section>
     </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const updateState = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/login", state)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <>
       <section className="login">
         <form className="authForm">
           <div className="inputs">
             <label htmlFor="">email: </label>
-            <input type="email" placeholder="Enter your email" />
+            <input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              onChange={updateState}
+            />
           </div>
           <div className="inputs">
             <label htmlFor="">password: </label>
-            <input type="password" placeholder="Enter your Password" />
+            <input
+              type="password"
+              placeholder="Enter your Password"
+              name="password"
+              onChange={updateState}
+            />
           </div>
         </form>
       </section>
